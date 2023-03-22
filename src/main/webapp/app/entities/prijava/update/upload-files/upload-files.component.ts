805b3695545db88c0abacafc7261451cbc780e12
deva56 <antonio.deva@hotmail.com>
2023-03-22 21:59:32 +0100

    Upload file promjene.

--- src/main/webapp/app/entities/prijava/update/upload-files/upload-files.component.ts
+++ src/main/webapp/app/entities/prijava/update/upload-files/upload-files.component.ts
@@ -8,6 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UploadFilesComponent implements OnInit {
   fileForm!: FormGroup;
+  selectedFiles!: FileList;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
@@ -17,22 +18,18 @@ export class UploadFilesComponent implements OnInit {
     });
   }
 
-  onSubmit() {
-    const formData = new FormData();
-    const files = this.fileForm.get('files')!.value;
-
-    if (!files || files.length === 0) {
-      console.log('No files selected');
+  uploadFiles(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    if (target.files !== null) {
+      this.selectedFiles = target.files;
+    } else {
+      console.log('No files selected!');
       return;
     }
+    const formData = new FormData();
 
-    for (let i = 0; i < files.length; i++) {
-      if (typeof files[i] !== 'object') {
-        console.log(`Skipping invalid file: ${files[i]}`);
-        continue;
-      }
-      formData.append('files', files[i], files[i].name);
-      console.log('File:', files[i].name);
+    for (let i = 0; i < this.selectedFiles.length; i++) {
+      formData.append('files', this.selectedFiles[i], this.selectedFiles[i].name);
     }
 
     const prijavaId = 1; // Replace with the ID of the Prijava instance
